5bd5f42f22f00a769254a274c3b8c5dee86fa386
Phillip Sanders <phj.sanders@gmail.com>
2019-12-07 17:07:58 -0600

    Testing graph 2

--- test.py
+++ test.py
@@ -2,16 +2,18 @@ import numpy as np
 
 def get_graph():
     edgelist = np.loadtxt(f'data/G1.txt', dtype = int)
-    G = ([],[])
+    G = ([],{})
 
     for x in edgelist:
-        print(x)
+        
         if x[0] not in G[0]:
             G[0].append(x[0])
-        
+    
         if x[1] not in G[0]:
-            G[1].append( ( x[0],x[1] ) )
+            G[0].append(x[1])
 
+        G[1][(x[0], x[1])] = x[2]
+        
     return G
 
 graph = get_graph()
@@ -19,3 +21,4 @@ graph = get_graph()
 print(f'V(G) = {graph[0]}')
 print('')
 print(f'E(G) = {graph[1]}')
+
