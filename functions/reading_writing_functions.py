ee1a92543492532dc7903839df6bd67ee8e7508e
Phillip Sanders <phj.sanders@gmail.com>
2019-12-08 13:24:37 -0600

    making more readable and commenting

--- functions/reading_writing_functions.py
+++ functions/reading_writing_functions.py
@@ -3,15 +3,15 @@ import numpy as np
 ##Function takes the graph from data and returns a tuple of its Vertices and the edges with thier weight values
 def get_graph(textfile):
     edgelist = np.loadtxt(f'data/{textfile}', dtype = int)
-    G = ([],{})
+    graph = ([],{})
 
-    for x in edgelist:                                  ##Iterates throught the list
-        if x[0] not in G[0]:                            ##adds the vertives as the first entry of the tuple
-            G[0].append(x[0])
+    for vertex in edgelist:                                     ##iterates throught the list
+        if vertex[0] not in graph[0]:                           ##adds the vertives as the first entry of the tuple
+            graph[0].append(vertex[0])
     
-        if x[1] not in G[0]:                            ##adds the last vertice
-            G[0].append(x[1])
+        if vertex[1] not in graph[0]:                           ##adds the last vertex
+            graph[0].append(vertex[1])
 
-        G[1][(x[0], x[1])] = x[2]                       #adds the edges and weights as the second value in the tuple
+        graph[1][(vertex[0], vertex[1])] = vertex[2]            ##adds the edges and weights as the second value in the tuple
         
-    return G
+    return graph                                                ##returns tuple of vertices and edges with weights
