604609a60d0d7c090df4e45eb9101568c42de38e
Phillip Sanders <phj.sanders@gmail.com>
2019-12-09 15:32:43 -0600

    Testing algorithm with new data

--- algorithms/prims_algorithm.py
+++ algorithms/prims_algorithm.py
@@ -4,9 +4,10 @@ from functions.graph_operations import incident_edges
 from functions.graph_operations import edge_cost
 
 ##returns tree with shortest non-looping of a graph using the starting vertex and an empty tree
-def prims_alg(graph,x):
+def prims_mst_alg(graph,x):
 
-    vertex = init_tuple(x)
+    vertex = init_tuple(x)                                          ##creates tuple of starting vertex and a blank tree
+    mstSum = 0                                                      ##initializes minimum spanning tree sum
 
     while sorted(vertex[0])!=sorted(graph[0]):                      ##continues until all vertices are a member of the tree
         
@@ -15,7 +16,7 @@ def prims_alg(graph,x):
         minimumCostEdge = edges[0]                                  ##sets edge with initial minimum cost (first incident edge)
 
         for edge in edges:                                          ##iterates through all incident edges
-            if edge_cost(graph,edge) <  minimumCost:                ##checks if the initial minimum cost is the actual minimum cost
+            if edge_cost(graph,edge) <=  minimumCost:               ##checks if the initial minimum cost is the actual minimum cost
                 minimumCost = edge_cost(graph,edge)                 ##replaces initial minimum cost with true minimum cost
                 minimumCostEdge = edge                              ##replaces edge with initial minimum cost with the true edge of minimum cost
 
@@ -25,4 +26,7 @@ def prims_alg(graph,x):
             if edgePoint not in vertex[0]:                          ##checks to see if the next vertex is currently in the vertex list
                 vertex[0].append(edgePoint)                         ##adds vertex to the vertex list
 
-    return vertex                                                   ##returns minimum spanning tree
+    for mstEdge in vertex[1]:                                       ##iterates through MST edges
+            mstSum += edge_cost(graph , mstEdge)                    ##sums the cost of each edge
+
+    return vertex[1], mstSum                                        ##returns tuple containing minimum spanning tree and cost sum
--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from functions.reading_writing_functions import get_graph
 from functions.graph_operations import init_tuple
 from functions.graph_operations import incident_edges
 from functions.graph_operations import edge_cost
-from algorithms.prims_algorithm import prims_alg
+from algorithms.prims_algorithm import prims_mst_alg
 
-graph = get_graph("G1")                                 ##reads graph with filename and writes the edges and vertices with their weights
+graph = get_graph("G2")                                 ##reads graph with filename and writes the edges and vertices with their weights
 
 print(f'Graph vertices = {graph[0]}')                   ##Prints vertices
 print('')
@@ -22,19 +22,16 @@ cost = edge_cost(graph, (2, 3))
 print('')
 print(f'The cost of edge (2, 3) is: {cost}')
 
-vertexEdgeCost = edge_cost(graph, vertexEdges[1])
+vertexEdgeCost = edge_cost(graph, vertexEdges[0])
 
 print('')
-print(f'The cost of edge {vertexEdges[1]} is: {vertexEdgeCost}')
+print(f'The cost of edge {vertexEdges[0]} is: {vertexEdgeCost}')
 
-minSpanningTree = prims_alg(graph, 0)
-
-print('')
-print(f'{minSpanningTree}')
-
-testTriplette = ([0],[], 0)
-testTriplette[1].append(1)
-
+minSpanningTree = prims_mst_alg(graph, 1)
 
 print('')
-print(type(testTriplette[2]))
+print(minSpanningTree)
+
+print('')
+print(f'Minimum spanning tree: {minSpanningTree[0]}')
+print(f'Minimum spanning tree cost: {minSpanningTree[1]}')
