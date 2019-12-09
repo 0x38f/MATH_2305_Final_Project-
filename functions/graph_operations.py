82af35153197342578ac6f791d2bfb174030c83f
Phillip Sanders <phj.sanders@gmail.com>
2019-12-09 10:37:48 -0600

    Added edge_cost function and tested

--- functions/graph_operations.py
+++ functions/graph_operations.py
@@ -1,19 +1,23 @@
-##takes a integer and creates a tuple
+##takes an integer and creates a tuple
 def init_tuple(x):
     return ([x],[])
 
 ##finds all edges a vertex is a member of
 def incident_edges(graph, x):
 
-    vertex_tuple = init_tuple(x)
+    vertexTuple = init_tuple(x)
     edges = []                                                                                  ##initializes list for edges
 
     for edge in graph[1]:                                                                       ##iterates through all graph edges
-        for vertex in vertex_tuple[0]:                                                          ##iterates until vertex
+        for vertex in vertexTuple[0]:                                                           ##iterates until vertex
             if vertex in edge:                                                                  ##checks if vertex is a member of the edge
-                if edge not in vertex_tuple[1]:                                                 ##checks if edge is in vertcies tuple
-                    if ((edge[0] not in vertex_tuple[0]) or (edge[1] not in vertex_tuple[0])):  ##checks if graph edge has been added
+                if edge not in vertexTuple[1]:                                                  ##checks if edge is in vertcies tuple
+                    if ((edge[0] not in vertexTuple[0]) or (edge[1] not in vertexTuple[0])):    ##checks if graph edge has been added
                         
                         edges.append(edge)                                                      ##adds graph edge to edges list
 
     return edges                                                                                ##returns list of edges that vertex is a member of
+
+##takes a graph and an edge in the graph, returns the cost of the edge
+def edge_cost(graph,edge):
+    return graph[1][edge]
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from functions.reading_writing_functions import get_graph
 from functions.graph_operations import incident_edges
+from functions.graph_operations import edge_cost
 
 graph = get_graph("G1")                                 ##reads graph with filename and writes the edges and vertices with their weights
 
@@ -13,5 +14,15 @@ vertexEdges = incident_edges(graph, vertex)
 
 print('')
 print(f'{vertex} is a member of edges {vertexEdges}')
+
+cost = edge_cost(graph, (2, 3))
+
+print('')
+print(f'The cost of edge (2, 3) is: {cost}')
+
+vertexEdgeCost = edge_cost(graph, vertexEdges[1])
+
+print('')
+print(f'The cost of edge {vertexEdges[1]} is: {vertexEdgeCost}')
 
 
